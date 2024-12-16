6c0e26cd69369db7e66942eab338c16d5c3d43e0
itznokx <dimitri.b.medeiros@gmail.com>
2024-12-16 13:16:06 -0300

    Update printer.c & writer.c

--- semaforo/printer.c
+++ semaforo/printer.c
@@ -1,48 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <pthread.h>
 #include <unistd.h>
+#include <pthread.h>
 
+//semÃ¡foros
 #include <semaphore.h>
 #include <fcntl.h>
 
-//global semaphore
-sem_t *sem;
-void* imprime(void *args){
-	int in = 0, out = 0;
+sem_t* sem;
+
+void *imprime(void* args)
+{
+    int in = 0, out = 0;
 	while(1)
 	{
-		//Espera o semÃ¡foro ficar verde para continuar. Antes de entrar na regiÃ£o crÃ­tica, coloca vermelho (0)
 		sem_wait(sem);
-		
-		//***InÃ­cio da regiÃ£o crÃ­tica***, pois manipula arquivo compartilhado com a printer
-		
-		//abre arquivo com informaÃ§Ãµes do spooler, lendo variÃ¡veis in (Ã­ndice do arquivo a ser gravado) e out (Ã­ndice do arquivo a ser impresso)
 		FILE* spool;
-	    while(!(spool = fopen("spool.txt","r+")));		
+
+		spool = fopen("spool.txt","r+");
 	    fscanf(spool, "%d %d\n", &in, &out);
 
-		//incrementa out para ter Ã­ndice do arquivo lido (o out sempre aponta para o Ã­ndice "anterior", de tal forma que, se ele Ã© negativo, a impressora sabe que jÃ¡ imprimiu tudo)
 		out++;
 		
-		//se o out for vÃ¡lido, entra na rotina de impressÃ£o
 	    if(out < in && out >= 0)
 	    {
-	    	//gera o nome do arquivo a imprimir
-			char filename[20];	
-			sprintf(filename, "%d.txt", out);
+			char filename[20];
 	
-			//abre arquivo a imprimir
+			sprintf(filename, "%d.prt", out);
+	
             FILE *fout;
-            while(!(fout = fopen(filename, "r")));
+            fout = fopen(filename, "r");
 		
-			//imprime arquivo
 			char content[100];
 			fscanf(fout, "%s", content);
 			printf("%s\n", content);
 
-			//fecha arquivo e remove
 			fclose(fout);
 			remove(filename);
 		}else{
@@ -52,39 +45,48 @@ void* imprime(void *args){
 			
 		}
 
-		//atualiza spool com novos in e out (out incrementado para apontar para o prÃ³ximo arquivo a ser impresso)
 		rewind(spool);
     	fprintf(spool, "%d %d\n", in, out);		
+
 		fclose(spool);
-		
-		//*** Fim da regiÃ£o crÃ­tica ***
-		//passa semÃ¡foro para verde (1) para que outro processo que tambÃ©m utiliza esse sinal possa entrar na sua regiÃ£o crÃ­tica
 		sem_post(sem);
 		
-		//Se imprimiu tudo (out = -1), durma
 		//if(out == -1) raise(SIGSTOP);
 	}
 	return NULL;
 }
-void* menu(void *args){
+
+void* menu(void* arg)
+{
 	while(getchar() != 'q');
 	sem_close(sem);
-	sem_unlink("/sem_printer");
+	sem_unlink("/sem_print");
 	exit(0);
 	return NULL;
 }
+
 int main()
 {
+
+	
 	pid_t pid = getpid();
-	FILE* pidfile = fopen("pidprintex.txt","w");
-	fprintf(pidfile, "%d", pid );
-	sem_unlink("/sem_printer");
-	sem = sem_open("/sem_printer",O_CREAT | O_EXCL, 0644,1);
-	pthread_t thread1,thread2;
+	
+	FILE* spid = fopen("spid.txt", "w");
+	fprintf(spid, "%d", pid);
+	fclose(spid);
+	
+	sem = sem_open("/sem_print", O_CREAT | O_EXCL, 0644, 1);
+	if(sem == SEM_FAILED){
+	 	sem_unlink("/sem_print");
+	 	sem = sem_open("/sem_print", O_CREAT | O_EXCL, 0644, 1);
+	}
 
-	pthread_create(&thread1,NULL,imprime,NULL);
-	pthread_create(&thread2,NULL,menu,NULL);
+	pthread_t thread, thread2;
+	pthread_create(&thread, NULL, imprime, NULL);
+	pthread_create(&thread2, NULL, menu, NULL);
+	
+	pthread_join(thread, NULL);
+	pthread_join(thread2, NULL);
 
-	pthread_join(thread1,NULL);
-	pthread_join(thread2,NULL);
+	return 0;
 }
--- semaforo/writer.c
+++ semaforo/writer.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
 
+//semÃ¡foros
 #include <semaphore.h>
 #include <fcntl.h>
 
@@ -9,30 +11,25 @@ int main()
 {
 
 	int in = 0, out = 0;
-    FILE* spool;
-    
-    //Abre semÃ¡foro criado pelo printer
-    sem_t *sem = sem_open("/sem_printer", O_RDWR);
 
-	//laÃ§o infinito para pegar texto e mandar para impressÃ£o
+	sem_t* sem = sem_open("/sem_print", O_RDWR);
+	if(sem == SEM_FAILED) exit(1);
+
 	while(1)
 	{
-		//Espera usuÃ¡rio digitar texto e o lÃª
 		char texto[100];
 		scanf("%s", texto);
-		
-		//Espera o semÃ¡foro ficar verde para continuar. Antes de entrar na regiÃ£o crÃ­tica, coloca vermelho (0)
-		sem_wait(sem);
+	
 
-		//***InÃ­cio da regiÃ£o crÃ­tica***, pois manipula arquivo compartilhado com a printer
-		
-		//abre arquivo com informaÃ§Ãµes do spooler, lendo variÃ¡veis in (Ã­ndice do arquivo a ser gravado) e out (Ã­ndice do arquivo a ser impresso)
-		while(!(spool = fopen("spool.txt","r+")));		
+    	sem_wait(sem);
+	
+    	FILE* spool;
+		spool = fopen("spool.txt","r+");		
 		fscanf(spool, "%d %d\n", &in, &out);
 		
-		//cria arquivo de impressÃ£o com a string digitada pelo usuÃ¡rio. O nome do arquivo Ã© igual ao Ã­ndice de entrada do spooler
 		char filename[20];
-		sprintf(filename, "%d.txt", in);
+		sprintf(filename, "%d.prt", in);
+		
 		
 		FILE* fout = fopen(filename, "w");
 		
@@ -40,22 +37,19 @@ int main()
 		
 		fclose(fout);
 	
-		//atualiza spooler com o Ã­ndice da prÃ³xima posiÃ§Ã£o livre
 		rewind(spool);
     	fprintf(spool, "%d %d\n", (in+1), out);
-		fclose(spool);
-    	
-    	//acorda processo printer lendo o seu nÃºmero no arquivo
-    	int pid;
-    	FILE* pidf = fopen("pidprinter.txt", "r");
-    	fscanf(pidf, "%d", &pid);
-    	kill(pid, SIGCONT);
-    	fclose(pidf);
 		
-		//*** Fim da regiÃ£o crÃ­tica ***
-		//passa semÃ¡foro para verde (1) para que outro processo que tambÃ©m utiliza esse sinal possa entrar na sua regiÃ£o crÃ­tica
+		fclose(spool);
 		sem_post(sem);
-	}
 	
+		pid_t pid;
+		FILE* spid = fopen("spid.txt", "r");
+		fscanf(spid, "%d", &pid);
+		fclose(spid);
+
+		//kill(pid, SIGCONT);
+	}		
+
 	return 0;
 }
