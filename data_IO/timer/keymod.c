0c3a180d725e259dfb8bd6fd0da16fe556451bf5
Dimitri Medeiros <79457494+itznokx@users.noreply.github.com>
2025-01-17 11:15:52 -0300

    Create keymod.c

---
+++ data_IO/timer/keymod.c
@@ -0,0 +1,25 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/input.h>
+#include <linux/jiffies.h>
+#include <linux/timer.h>
+#include <linux/slab.h>
+
+struct vkeyboard 
+{
+	struct input_dev* inputdev;
+	struct timer_list keyboardtimer;
+};
+
+static void tratainterrupt (struct timer_list* t)
+{
+	struct vkeyboard* vkb = from_timer(vkb, t, keyboardtimer);
+
+	input_report_key (vkb->inputdev,KEY_A,1);
+	input_sync(vkb->inputdev);
+
+	input_report_key(vkb->inputdev,KEY_A,0);
+	input_sync(vkb->inputdev);
+
+	mod_timer (&vkb->keyboardtimer,jiffies+1000);
+}
