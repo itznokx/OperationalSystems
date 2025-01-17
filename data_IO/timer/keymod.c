3cf8b83cbc4b2852554ca75b535cbe418e8f032d
itznokx <dimitri.b.medeiros@gmail.com>
2025-01-17 09:12:37 -0300

    keymod.c

--- data_IO/timer/keymod.c
+++ data_IO/timer/keymod.c
@@ -31,4 +31,36 @@ static int __init vkeyboard_init (void)
 	vkb = kzalloc(sizeof(struct vkeyboard),GFP_KERNEL);
 
 	vkb->inputdev = input_allocate_device();
+	vkb->inputdev->name = "Teclado relógio";
+	vkb->inputdev->phys = "ps2/input0";
+	vkb->inputdev->id.bustype = BUS_USB;
+	vkb->inputdev->id.vendor = 0x1234;
+	vkb->inputdev->id.product = 0x5670;
+
+	set_bit(EV_KEY,vkb->inputdev->evbit);
+	set_bit(KEY_A,vkb->inputdev->keybit);
+
+	input_register_device(vkb->inputdev);
+
+	timer_setup(&vkb->keyboardtimer,tratainterrupt, 0);
+	mod_timer(&vkb->keyboardtimer, jiffies+ 1000);
+
+	return 0;
 }
+
+static void __exit vkeyboar_exit(void)
+{
+	static vkeyboard* vkb;
+
+	del_timer_sync(&vkb->keyboardtimer);
+
+	input_unregister_device(vkb->inputdev);
+
+	kfree(vkb);
+}
+module_init (vkeyboard_init);
+module_exit (vkeyboar_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("SO");
+MODULE_DESCRIPTION("VIRTUAL KEYBOARD");
